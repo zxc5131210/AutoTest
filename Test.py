ce3a93e30bd6cd29f1bba722df05f0e7d5fa934f
ian.wu <ian.viewsonic@gmail.com>
2023-09-18 09:28:55 +0800

    change recent app logic

--- Test.py
+++ Test.py
@@ -1,15 +1,6 @@
 import uiautomator2 as u2
-
+import time
 
 d = u2.connect()
-d.keyevent("home")
-d(resourceId='com.viewsonic.sidetoolbar:id/arrow').click()
-d(resourceId='com.viewsonic.sidetoolbar:id/btnAllTools').click()
-d.xpath('//*[@text="Spotlight"]').click()
-d().pinch_in(percent=10, steps=10)
-d(resourceId='com.viewsonic.sidetoolbar:id/settings_btn').click()
-d(resourceId='com.viewsonic.sidetoolbar:id/seekbar_alpha').swipe("left")
-d().swipe('left')
-d().swipe('up')
-d().swipe('right')
-d().swipe('down')
+ele = d(resourceId='com.viewsonic.vlauncher:id/remove_task')
+ele.click()

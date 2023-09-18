7cc9bf68fd9a614dc63b7b9d792ad76b51593cf2
ian.wu <ian.viewsonic@gmail.com>
2023-09-19 07:56:27 +0800

    stopwatch_moving

--- Test.py
+++ Test.py
@@ -3,7 +3,20 @@ import time
 
 d = u2.connect()
 
-ele = d(textContains="Manager")
-if ele.exists:
-    element.swipe("left")
-    print('123')
+# ele = d.xpath('//*[@resource-id="com.viewsonic.sidetoolbar:id/hour_wheelview"]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.TextView[1]')
+# print(ele.text)
+
+target_text = '10'
+target_scrollbar = 'com.viewsonic.sidetoolbar:id/hour_wheelview'
+ele = '//*[@resource-id="com.viewsonic.sidetoolbar:id/hour_wheelview"]/android.widget.FrameLayout[2]/android.widget.LinearLayout[1]/android.widget.TextView[1]'
+# 循环滑动并查找文本
+for _ in range(60):
+    if d.xpath(ele).text == target_text:
+        # 找到文本后执行操作，例如点击元素
+        break  # 找到后跳出循环
+    else:
+        # 如果未找到文本，继续滑动
+        d(resourceId=target_scrollbar).swipe('up')
+
+# 如果循环结束仍未找到文本，可以添加适当的处理
+51

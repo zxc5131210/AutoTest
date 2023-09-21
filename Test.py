a47053ec269c66cbf163c6461d50b12ddc68ef76
ian.wu <ian.viewsonic@gmail.com>
2023-09-21 10:54:01 +0800

    change delete temporarily screenshot logic

--- Test.py
+++ Test.py
@@ -5,11 +5,4 @@ import os
 # 初始化连接到设备
 d = u2.connect()
 
-# d.screenshot("QAQ.png")
-screenshot = './QAQ.png'
-
-current_directory = os.getcwd()
-for filename in os.listdir(current_directory):
-    filepath = os.path.join(current_directory, filename)
-    if os.path.isfile(screenshot):
-        os.remove(screenshot)
+d.screen_off()

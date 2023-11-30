7cb0122447fe9e3ceac5816d4122038ee00635a9
ian.wu <ian.viewsonic@gmail.com>
2023-11-30 16:55:20 +0800

    different resolution wallpaper testcase

--- option_file/Wallpaper/option.py
+++ option_file/Wallpaper/option.py
@@ -7,6 +7,12 @@ class WallPaper(item_strategy.Strategy):
         "0": "Back to main menu",
         "1": "Change By_default",
         "2": "Change By_update",
+        "3": "480p wallpaper",
+        "4": "720p wallpaper",
+        "5": "1080p wallpaper",
+        "6": "2k wallpaper",
+        "7": "4k wallpaper",
+        "8": "8k wallpaper",
         "all": "all Test",
     }
     folder_path = "option_file/Wallpaper"
@@ -30,10 +36,64 @@ class WallPaper(item_strategy.Strategy):
         self.reporter.add_category("wallpaper")
         self.reporter.test_case("Change wallpaper to update image")
 
+    def _wallpaper_480p(self):
+        self.event_gen.generate_event(
+            json_path=f"{self.folder_path}/change_by_480p.json",
+            driver=self.driver,
+        )
+        self.reporter.add_category("wallpaper")
+        self.reporter.test_case("Change to 480p wallpaper")
+
+    def _wallpaper_720p(self):
+        self.event_gen.generate_event(
+            json_path=f"{self.folder_path}/change_by_720p.json",
+            driver=self.driver,
+        )
+        self.reporter.add_category("wallpaper")
+        self.reporter.test_case("Change to 720p wallpaper")
+
+    def _wallpaper_1080p(self):
+        self.event_gen.generate_event(
+            json_path=f"{self.folder_path}/change_by_1080p.json",
+            driver=self.driver,
+        )
+        self.reporter.add_category("wallpaper")
+        self.reporter.test_case("Change to 1080p wallpaper")
+
+    def _wallpaper_2k(self):
+        self.event_gen.generate_event(
+            json_path=f"{self.folder_path}/change_by_2k.json",
+            driver=self.driver,
+        )
+        self.reporter.add_category("wallpaper")
+        self.reporter.test_case("Change to 2k wallpaper")
+
+    def _wallpaper_4k(self):
+        self.event_gen.generate_event(
+            json_path=f"{self.folder_path}/change_by_4k.json",
+            driver=self.driver,
+        )
+        self.reporter.add_category("wallpaper")
+        self.reporter.test_case("Change to 4k wallpaper")
+
+    def _wallpaper_8k(self):
+        self.event_gen.generate_event(
+            json_path=f"{self.folder_path}/change_by_8k.json",
+            driver=self.driver,
+        )
+        self.reporter.add_category("wallpaper")
+        self.reporter.test_case("Change to 8k wallpaper")
+
     def run_all(self):
         self.reporter.test_title("---Wallpaper---")
         self._wallpaper_by_default()
         self._wallpaper_by_update()
+        self._wallpaper_480p()
+        self._wallpaper_720p()
+        self._wallpaper_1080p()
+        self._wallpaper_2k()
+        self._wallpaper_4k()
+        self._wallpaper_8k()
 
     def run(self):
         while True:
@@ -47,6 +107,18 @@ class WallPaper(item_strategy.Strategy):
                     self._wallpaper_by_default()
                 case "2":
                     self._wallpaper_by_update()
+                case "3":
+                    self._wallpaper_480p()
+                case "4":
+                    self._wallpaper_720p()
+                case "5":
+                    self._wallpaper_1080p()
+                case "6":
+                    self._wallpaper_2k()
+                case "7":
+                    self._wallpaper_4k()
+                case "8":
+                    self._wallpaper_8k()
                 case "all":
                     self.run_all()
                 case _:

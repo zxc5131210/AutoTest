565138366aeb404a3ea756ec28789f9e4b73658a
ian.wu <ian.viewsonic@gmail.com>
2024-01-08 16:24:43 +0800

    50-5 a13 remote controller map(目前因為權限問題無法使用)

--- remote_controller_map.py
+++ remote_controller_map.py
@@ -10,3 +10,15 @@ ifp33_keycode = {
     "back": "15",
     "mute": "113",
 }
+ifp50_5_a13_keycode = {
+    "input_event": "0",
+    "home": "102",
+    "quicksettings": "466",
+    "settings": "139",
+    "close_backlight": "640",
+    "volume_up": "115",
+    "volume_down": "114",
+    "whiteboard": "641",
+    "back": "15",
+    "mute": "113",
+}

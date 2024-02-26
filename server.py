1b0f373a05ebee5299920a60ae26c5f58ab31499
zxc5131210 <zxc5131210@gmail.com>
2024-02-26 08:45:44 +0800

    add error handling and keep the server to do next release testing.

--- server.py
+++ server.py
@@ -1,28 +1,44 @@
 import socket
+import logging
 import ci_script
 
 
 def start_server():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)-2s %(message)s",
+        datefmt="%Y%m%d %H:%M:%S",
+    )
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # blind ip address and port
-    server_address = ("", 12345)
-    server_socket.bind(server_address)
+    try:
+        # bind ip address and port
+        server_address = ("", 12345)
+        server_socket.bind(server_address)
 
-    # start listen
-    server_socket.listen(1)
+        # start listening
+        server_socket.listen(1)
 
-    while True:
-        # waiting for client
-        print("Waiting for a connection...")
-        client_socket, client_address = server_socket.accept()
-        # get the client data
-        data = client_socket.recv(1024)
-        if data.decode():
-            start_ci_process()
-            # close client
-            client_socket.close()
-            break
+        while True:
+            # waiting for a connection
+            logging.info("Waiting for a connection...")
+            client_socket, client_address = server_socket.accept()
+            try:
+                # get the client data
+                data = client_socket.recv(1024)
+                if data.decode():
+                    start_ci_process()
+            except Exception as e:
+                logging.error(f"Error processing client data: {e}")
+            finally:
+                # close client
+                client_socket.close()
+
+    except Exception as e:
+        logging.error(f"Error in server: {e}")
+    finally:
+        server_socket.close()
 
 
 def start_ci_process():
